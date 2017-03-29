f9b15dabae5a220b905e5db7753a8aea12955278
Vlad Antohi <vlad@bluechilli.com>
2017-03-29 13:04:28 +1100

    started renaming

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyProduct("ChilliSource Core")]
+[assembly: AssemblyProduct("ChilliSource Mobile Core")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0-alpha.26+Branch.develop.Sha.8b4f23dac8d3e37221cecb4d9a9ae954435ad6cc")]
